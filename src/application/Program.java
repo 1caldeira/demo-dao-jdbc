5169da5b4c6c847195c5f17857de2d488d8273e4
Gabriel Caldeira <g.caldeiraa1@gmail.com>
2023-08-09 12:30:37 -0300

    findById Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,10 +10,10 @@ import java.util.Date;
 public class Program {
 
     public static void main(String[] args) {
-        Department department = new Department(12, "Books");
-        System.out.println(department);
-        Seller seller = new Seller(2,"Gabriel","gabriel@gmail.com", new Date(),3000.0,department);
-        System.out.println(seller);
+
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
+
     }
 }
