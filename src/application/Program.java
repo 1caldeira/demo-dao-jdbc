11e231fd532671a4637589069a639539c875341d
Gabriel Caldeira <g.caldeiraa1@gmail.com>
2023-08-10 11:06:26 -0300

    findByDepartment Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,15 +5,24 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Program {
 
     public static void main(String[] args) {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        System.out.println("==== TEST 1: seller findById ====");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+        System.out.println("==== TEST 2: seller findByDepartment ====");
+        List<Seller> sellers;
+        sellers = sellerDao.findByDepartment(seller.getDepartment());
+        for (Seller obj : sellers) {
+            System.out.println(obj);
+        }
 
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -10,6 +10,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
@@ -21,7 +22,6 @@ public class SellerDaoJDBC implements SellerDao {
     }
 
     public SellerDaoJDBC() {
-
     }
 
     @Override
@@ -53,16 +53,8 @@ public class SellerDaoJDBC implements SellerDao {
             st.setInt(1, id);
             rs = st.executeQuery();
             if(rs.next()){
-                Department dep = new Department();
-                dep.setId(rs.getInt("DepartmentId"));
-                dep.setName(rs.getString("depName"));
-                Seller obj = new Seller();
-                obj.setId(rs.getInt("Id"));
-                obj.setName(rs.getString("Name"));
-                obj.setEmail(rs.getString("Email"));
-                obj.setBirthDate(rs.getDate("BirthDate"));
-                obj.setBaseSalary(rs.getDouble("BaseSalary"));
-                obj.setDepartment(dep);
+                Department dep = instantiateDepartment(rs);
+                Seller obj = instantiateSeller(rs, dep);
                 return obj;
             }
         } catch (SQLException e) {
@@ -72,6 +64,53 @@ public class SellerDaoJDBC implements SellerDao {
             DB.closeResultSet(rs);
         }
         return null;
+    }
+
+    public List<Seller> findByDepartment(Department department){
+        PreparedStatement st = null;
+        ResultSet rs = null;
+        try{
+            st = conn.prepareStatement(
+                    "SELECT seller.*,department.Name as depName "
+                            + "FROM seller INNER JOIN department "
+                            +"ON seller.DepartmentId = department.Id "
+                            +"WHERE DepartmentId = ? "
+                            +"ORDER BY Name");
+
+            st.setInt(1, department.getId());
+            List<Seller> list = new ArrayList<>();
+            rs = st.executeQuery();
+            while(rs.next()){
+                Seller obj = instantiateSeller(rs, department);
+                list.add(obj);
+            }
+            return list;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }finally{
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
+    }
+
+
+
+    private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException{
+        Seller obj = new Seller();
+        obj.setId(rs.getInt("Id"));
+        obj.setName(rs.getString("Name"));
+        obj.setEmail(rs.getString("Email"));
+        obj.setBirthDate(rs.getDate("BirthDate"));
+        obj.setBaseSalary(rs.getDouble("BaseSalary"));
+        obj.setDepartment(dep);
+        return obj;
+    }
+
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("DepartmentId"));
+        dep.setName(rs.getString("depName"));
+        return dep;
     }
 
     @Override
