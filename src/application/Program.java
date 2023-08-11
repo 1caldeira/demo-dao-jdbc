f887f13991dfcc4e228770767edb8bdd48a1a553
Gabriel Caldeira <g.caldeiraa1@gmail.com>
2023-08-11 14:18:53 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.sql.SQLOutput;
 import java.util.Date;
 import java.util.List;
 
@@ -27,10 +28,18 @@ public class Program {
         for (Seller obj : sellers) {
             System.out.println(obj);
         }
-        System.out.println("==== TEST 4: seller insert ====");
+        /*System.out.println("==== TEST 4: seller insert ====");
         Seller newSeller = new Seller(null,"Greg Doucette","greg@gmail.com", new Date(),4000.0,new Department(1, null));
         sellerDao.insert(newSeller);
-        System.out.println("New Seller inserted! New ID = "+newSeller.getId());
+        System.out.println("New Seller inserted! New ID = "+newSeller.getId());*/
 
+        System.out.println("==== TEST 4: seller update ====");
+        seller = sellerDao.findById(9);
+        System.out.println("Before Update: ");
+        System.out.println(sellerDao.findById(9));
+        seller.setName("Greg Couchette");
+        sellerDao.update(seller);
+        System.out.println("Update completed!: ");
+        System.out.println(sellerDao.findById(9));
     }
 }
