e0579753a380b9893db2aafc3d994d99a1bdae51
Gabriel Caldeira <g.caldeiraa1@gmail.com>
2023-08-11 14:43:43 -0300

    delete and updateId implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,38 +8,70 @@ import model.entities.Seller;
 import java.sql.SQLOutput;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        System.out.println();
+
         System.out.println("==== TEST 1: seller findById ====");
         Seller seller = sellerDao.findById(3);
         System.out.println(seller);
+        System.out.println();
+
         System.out.println("==== TEST 2: seller findByDepartment ====");
         List<Seller> sellers;
         sellers = sellerDao.findByDepartment(seller.getDepartment());
         for (Seller obj : sellers) {
             System.out.println(obj);
         }
+        System.out.println();
+
         System.out.println("==== TEST 3: seller findAll ====");
         sellers = sellerDao.findAll();
         for (Seller obj : sellers) {
             System.out.println(obj);
         }
-        /*System.out.println("==== TEST 4: seller insert ====");
+        System.out.println();
+
+        System.out.println("==== TEST 4: seller insert ====");
         Seller newSeller = new Seller(null,"Greg Doucette","greg@gmail.com", new Date(),4000.0,new Department(1, null));
         sellerDao.insert(newSeller);
-        System.out.println("New Seller inserted! New ID = "+newSeller.getId());*/
+        System.out.println("New Seller inserted! New ID = "+newSeller.getId());
+        System.out.println();
 
-        System.out.println("==== TEST 4: seller update ====");
+        System.out.println("==== TEST 5: seller update ====");
         seller = sellerDao.findById(9);
         System.out.println("Before Update: ");
         System.out.println(sellerDao.findById(9));
-        seller.setName("Greg Couchette");
+        seller.setName("Greg Grette");
         sellerDao.update(seller);
         System.out.println("Update completed!: ");
         System.out.println(sellerDao.findById(9));
+        System.out.println();
+
+        System.out.println("==== TEST 6: seller delete ====");
+        System.out.println("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed");
+        System.out.println();
+
+        System.out.println("================Bonus feature================");
+        System.out.println();
+        System.out.println("==== TEST 7: Update Seller Id ====");
+        seller = sellerDao.findById(9);
+        System.out.println("Before Update: ");
+        System.out.println(sellerDao.findById(9));
+        seller.setId(8);
+        sellerDao.updateId(seller);
+        System.out.println("Update completed!: ");
+        System.out.println(sellerDao.findById(8));
+
+
+        sc.close();
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -79,8 +79,42 @@ public class SellerDaoJDBC implements SellerDao {
 
     }
 
+    public void updateId(Seller obj) {
+        PreparedStatement st = null;
+        try{
+            st = conn.prepareStatement(
+                    "UPDATE seller\n"
+                            + "SET Id = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ?\n"
+                            + "WHERE Name = ?", Statement.RETURN_GENERATED_KEYS);
+            st.setInt(1, obj.getId());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+            st.setDouble(4, obj.getBaseSalary());
+            st.setInt(5, obj.getDepartment().getId());
+            st.setString(6,obj.getName());
+            st.executeUpdate();
+        }catch(SQLException e){
+            throw new DbException(e.getMessage());
+        }finally{
+            DB.closeStatement(st);
+
+        }
+
+    }
+
     @Override
     public void deleteById(Integer id) {
+        PreparedStatement st = null;
+        try{
+            st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+            st.setInt(1, id);
+            st.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }finally{
+            DB.closeStatement(st);
+        }
 
     }
 
